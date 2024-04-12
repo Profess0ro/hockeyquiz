47f8aefb37579ae79abde93122efdf2f1a855cc3
Profess0ro <professoro88@gmail.com>
2024-04-12 19:20:31 +0200

    added const and functions to buttons

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,12 +1,30 @@
-document.addEventListener("DOMContentLoaded", function(){
-    let buttons = document.getElementsByTagName("button");{
+const playButton = document.getElementById('play-btn')
+const startButton = document.getElementById('startquiz')
+const questionContentArea = document.getElementById('game-area')
+const questionContent = document.getElementById('question')
+const answerButtons = document.getElementById('answerbutton')
+const welcomeArea = document.getElementById('welcome')
+const startGameArea = document.getElementById('startgame')
+const mainMenuButton = document.getElementById('mainmenu')
 
-    }
-});
+
+playButton.addEventListener("click", playQuiz)
+startButton.addEventListener("click", startGame)
+mainMenuButton.addEventListener("click", returnToMain)
 
 
-function startGame() {
-
+function playQuiz() {
+    welcomeArea.classList.add('hide')
+    startGameArea.classList.remove('hide')
+}
+function startGame () {
+    startGameArea.classList.add('hide')
+    questionContentArea.classList.remove('hide')
+}
+function returnToMain () {
+    questionContentArea.classList.add('hide')
+    startGameArea.classList.add('hide')
+    welcomeArea.classList.remove('hide')
 }
 function checkCorrectAnswer() {
 
@@ -18,5 +36,8 @@ function addWrongAnswer() {
 
 }
 function displayNextQuestion () {
+
+}
+function resetState () {
 
 }
