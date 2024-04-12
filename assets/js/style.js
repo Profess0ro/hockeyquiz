8179bd910184fa8dd7cf362f171199aeffcf64b5
Profess0ro <professoro88@gmail.com>
2024-04-12 19:34:58 +0200

    added functions to all buttons to show and hide the right divs

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,29 +1,30 @@
 const playButton = document.getElementById('play-btn')
-const startButton = document.getElementById('startquiz')
+const contactButton = document.getElementById('contact')
 const questionContentArea = document.getElementById('game-area')
 const questionContent = document.getElementById('question')
 const answerButtons = document.getElementById('answerbutton')
 const welcomeArea = document.getElementById('welcome')
-const startGameArea = document.getElementById('startgame')
+const contactArea = document.getElementById('contact-area')
 const mainMenuButton = document.getElementById('mainmenu')
 
 
 playButton.addEventListener("click", playQuiz)
-startButton.addEventListener("click", startGame)
+contactButton.addEventListener("click", showContact)
 mainMenuButton.addEventListener("click", returnToMain)
 
 
 function playQuiz() {
     welcomeArea.classList.add('hide')
-    startGameArea.classList.remove('hide')
-}
-function startGame () {
-    startGameArea.classList.add('hide')
     questionContentArea.classList.remove('hide')
+}
+function showContact () {
+    welcomeArea.classList.add('hide')
+    questionContentArea.classList.add('hide')
+    contactArea.classList.remove('hide')
 }
 function returnToMain () {
     questionContentArea.classList.add('hide')
-    startGameArea.classList.add('hide')
+    contactArea.classList.add('hide')
     welcomeArea.classList.remove('hide')
 }
 function checkCorrectAnswer() {
