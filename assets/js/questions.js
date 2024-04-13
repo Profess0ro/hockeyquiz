e243a75b6fec2a5fd7edc19b0a0e5c7f2d09d3f0
Profess0ro <professoro88@gmail.com>
2024-04-13 10:38:41 +0200

    added question

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -9,12 +9,21 @@ const questions = [
         ]
     },
     {
+        question: 'In a game between LA Kings and Montreal Canadiens, 5th of March 1988 a living animal was thrown into the ice from the public. What animal landed on the ice?',
+        answers: [
+            {text: 'Chicken', correct: true},
+            {text: 'Snake', correct: false},
+            {text: 'Rat', correct: false},
+            {text: 'Cat', correct: false},
+        ]
+    },
+ /**    {
         question: '',
         answers: [
-            {text: ''},
-            {text: ''},
-            {text: ''},
-            {text: ''},
+            {text: '', correct: },
+            {text: '', correct: },
+            {text: '', correct: },
+            {text: '', correct: }
         ]
-    }
+    }*/
 ]
