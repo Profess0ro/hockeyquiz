8d7282306107d022f5f61c252bd4da0507d82896
Profess0ro <professoro88@gmail.com>
2024-04-13 10:20:15 +0200

    added question.js

---
+++ assets/js/questions.js
@@ -0,0 +1,20 @@
+const questions = [
+    {
+        question: 'What made the goalie Joonas Korpisalo pause the game between Ottawa Senators and Washington Capitals on the 7th of april 2024?',
+        answers: [
+            {text: 'He needed to go to the bathroom', correct: false},
+            {text: 'He threw up', correct: false},
+            {text: 'He was blinded by the sun', correct: true},
+            {text: 'He lost his skate', correct: false}
+        ]
+    },
+    {
+        question: '',
+        answers: [
+            {text: ''},
+            {text: ''},
+            {text: ''},
+            {text: ''},
+        ]
+    }
+]
