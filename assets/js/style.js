d08b3eefb32a7ce178c978b140591a962154c72d
Profess0ro <professoro88@gmail.com>
2024-04-16 10:41:07 +0200

    adding functions to email form

--- assets/js/style.js
+++ assets/js/style.js
@@ -290,3 +290,24 @@ function resetScore() {
     correctScoreElement.innerText = "0";
     wrongScoreElement.innerText = "0";
 }
+
+function sendEmail() {
+    var params = {
+        name: document.getElementById("name").value ,
+        email: document.getElementById("email").value ,
+        message: document.getElementById("message").value ,
+
+    };
+const serviceID = "service_jmbyfsi";
+const templateID = "template_j35glvk";
+
+emailjs.send(serviceID,templateID,params)
+.then(
+    res =>{ 
+    document.getElementById("name").value = "",
+    document.getElementById("email").value = "",
+    document.getElementById("message").value = "";
+    alert("Thank you for the feedback")
+}
+)
+}
