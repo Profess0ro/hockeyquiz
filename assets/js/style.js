395cc5e11db917f9eeb15d423b17e9eefe3bbe73
Profess0ro <professoro88@gmail.com>
2024-04-22 21:08:00 +0200

    changed all comments from single lines to multilines

--- assets/js/style.js
+++ assets/js/style.js
@@ -19,12 +19,20 @@ let inputNameArea = document.getElementById("input-name");
 let userWin = document.getElementById("win");
 let quizWin = document.getElementById("loss");
 
-// This makes the value of how many questions shown and helps questions to be shuffled
+/** 
+ * This makes the value 
+ * of how many questions shown 
+ * and helps questions to be shuffled
+ * */
 
 let shuffledQuestions = null;
 let currentQuestionIndex = null;
 
-// What function will be called by default when you click on the different buttons
+/** 
+ * What function will be called 
+ * by default when you click 
+ * on the different buttons 
+ * */ 
 startTheGameButton.addEventListener("click", collectData);
 playButton.addEventListener("click", inputUserName);
 contactButton.addEventListener("click", showContact);
@@ -32,19 +40,33 @@ mainMenuButton.addEventListener("click", returnToMain);
 restartButton.addEventListener("click", DoYouWantToRestart);
 
 
-// When clicking next question it will call the function to add 1 to how many questions has been shown
+/** 
+ * When clicking next question 
+ * it will call the function to 
+ * add 1 to how many 
+ * questions has been shown
+ * */ 
 nextQuestionButton.addEventListener("click", () => {
     currentQuestionIndex++;
     displayNextQuestion();
 });
-// Sets the buttons eventlistener to original state so you don´t being ask to leave the game outside the game
+/** 
+ * Sets the buttons eventlistener 
+ * to original state so you don´t 
+ * being ask to leave the game 
+ * outside of the game 
+ * */
 function resetButtons() {
     mainMenuButton.removeEventListener("click", DoYouWantToLeaveMain);
     contactButton.removeEventListener("click", DoYouWantToLeaveContact);
     contactButton.addEventListener("click", showContact);
     mainMenuButton.addEventListener("click", returnToMain);
 }
-// Adding game status to buttons so you´ll be asked if you are sure to leave the game or not.
+/** 
+ * Adding game status to buttons 
+ * so you´ll be asked if you are 
+ * sure to leave the game or not. 
+ * */ 
 function setGameStatus() {
     restartButton.removeEventListener("click", inputUserName);
     mainMenuButton.removeEventListener("click", returnToMain);
@@ -53,42 +75,69 @@ function setGameStatus() {
     contactButton.addEventListener("click", DoYouWantToLeaveContact);
     restartButton.addEventListener("click", DoYouWantToRestart);
 }
-// Change which function to be called by restart button so you don´t get the question if you want to leave the game or not
+/** 
+ * Change which function to be called 
+ * by restart button so you don´t 
+ * get the question if you 
+ * want to leave the game or not 
+ * when game ended
+ * */ 
 function endGameRestart() {
     restartButton.addEventListener("click", inputUserName);
     restartButton.removeEventListener("click", DoYouWantToRestart);
 }
 
-// If there is question left in the game a window will ask if they are sure to leave
+/**
+ * If user want to restart the game 
+ * by pressing "Restart" they will 
+ * be asked to restart 
+ * or continue
+ * */ 
 function DoYouWantToRestart() {
     if (11 > currentQuestionIndex + 1) {
-        const confirmRestart = window.confirm("Are you sure you want to restart?");
+        const confirmRestart = window.confirm
+        ("Are you sure you want to restart?");
 
         if (confirmRestart) {
             inputUserName();
         }
     }
 }
-// If user want to go to quit the game and go to the contact page
+/**
+ * If user want to leave the game 
+ * by pressing "Contact" they will 
+ * be asked to go back to the contact page 
+ * or continue
+ * */ 
 function DoYouWantToLeaveContact() {
     if (11 > currentQuestionIndex + 1) {
-        const confirmLeave = window.confirm("Are you sure you want to leave the game?");
+        const confirmLeave = window.confirm
+        ("Are you sure you want to leave the game?");
         if (confirmLeave) {
             showContact();
         }
     }
 }
-// If user want to leave the game pressing "main menu" they will come back to the Main menu
+/**
+ * If user want to leave the game 
+ * by pressing "main menu" they will 
+ * be asked to go back to the Main menu 
+ * or continue
+ * */ 
 function DoYouWantToLeaveMain() {
-    if (11 > currentQuestionIndex + 1) { // If there is question left in the game a window will ask if they are sure to leave
-        const confirmLeave = window.confirm("Are you sure you want to leave the game?");
+    if (11 > currentQuestionIndex + 1) { 
+        const confirmLeave = window.confirm
+        ("Are you sure you want to leave the game?");
         if (confirmLeave) {
             returnToMain();
         }
     }
 }
 
-// When you press play, the right div will be shown
+/** 
+ * When you press play 
+ * the right div will be shown
+ * */ 
 function inputUserName() {
     welcomeArea.classList.add("hide");
     questionContentArea.classList.add("hide");
@@ -103,7 +152,12 @@ function inputUserName() {
     });
 }
 
-// This function will collect last finished game or show a message that no games have been registered
+/** 
+ * This function will collect 
+ * last finished game or show 
+ * a message that no games 
+ * have been registered
+ * */ 
 function collectRescentScore() {
     const player = localStorage.getItem("latestUser");
     const score = localStorage.getItem("latestScore");
@@ -122,23 +176,38 @@ function collectRescentScore() {
 }
 
 
-// This function collects the name filled in and that name shows at the scoreboard
+/** 
+ * This function collects 
+ * the name filled in and 
+ * that name shows up 
+ * at the scoreboard
+ * */ 
 function collectData() {
     const inputField = document.getElementById("your-name");
     const inputValue = inputField.value;
 
+/** 
+ * If you left the namefield empty 
+ * a warning will be displayed 
+ * and send you back.
+ * */ 
     if (inputValue === "") {
-        alert("You didn't fill in your name"); // If you left the namefield empty a warning will be displayed and send you back.
+        alert("You didn't fill in your name"); 
         inputUserName();
     } else {
-        playQuiz(); // Starts the game if you have filled in a name.
+        playQuiz(); 
     }
-    // this will put the name you´ve filled to the scoreboard
+ 
     const outputSpan = document.getElementById("username");
     outputSpan.innerText = inputValue;
 }
 
-// This function shows the right div when the game starts, changes buttons eventlisteners and resets the scoreboard
+/** 
+ * This function shows the right div 
+ * when the game starts
+ * changes buttons eventlisteners 
+ * and resets the scoreboard
+ * */ 
 function playQuiz() {
     setGameStatus();
     resetScore();
@@ -149,11 +218,19 @@ function playQuiz() {
     restartButton.classList.remove("hide");
     userWin.classList.add("hide");
     quizWin.classList.add("hide");
-    shuffledQuestions = questions.sort(() => Math.random() - 0.5); // this picks a random question from the array of questions to be shown
+/**
+ * This picks a random question 
+ * from the array of questions 
+ * to be shown
+ * */
+    shuffledQuestions = questions.sort(() => Math.random() - 0.5); 
     currentQuestionIndex = 0;
     displayNextQuestion();
 }
-// Hides and shows the right div when you navigate to contact page
+/** 
+ * Hides and shows the right div 
+ * when you navigate to contact page
+ * */ 
 function showContact() {
     resetButtons();
     welcomeArea.classList.add("hide");
@@ -165,7 +242,10 @@ function showContact() {
     quizWin.classList.add("hide");
 
 }
-// Hides and shows the right div when you navigate to main page
+/** 
+ * Hides and shows the right div 
+ * when you navigate to main page
+ * */ 
 function returnToMain() {
     resetButtons();
     questionContentArea.classList.add("hide");
@@ -176,7 +256,12 @@ function returnToMain() {
     userWin.classList.add("hide");
     quizWin.classList.add("hide");
 }
-//Before the next question will be shown all conditions of the previous question and buttons will be reset
+/**
+ * Before the next question 
+ * will be shown all conditions 
+ * of the previous question 
+ * and buttons will be reset
+ * */
 
 function displayNextQuestion() {
     resetState();
@@ -185,9 +270,16 @@ function displayNextQuestion() {
 
 let timer = null;
 
-/**This function will fetch the question and answer from the question array in question.js and
- * paste that text into both question div and the answering buttons.
- * It also starts a new timer that counts down from 20.
+/**
+ * This function will fetch 
+ * the question and answer 
+ * from the question array 
+ * in question.js and
+ * paste that text into 
+ * both question div and 
+ * the answering buttons.
+ * It also starts a new timer 
+ * that counts down from 30.
  */
 function showQuestion(questions) {
     let count = 30;
@@ -203,37 +295,57 @@ function showQuestion(questions) {
     timer = setInterval(function() {
         count--;
         document.getElementById("timer").innerText = count;
-        if (count === 0) { // Stop the timer when time runs out and add a wrong score if not answered in time.
+        /** 
+         * Stop the timer when time runs 
+         * out and add a wrong score 
+         * if not answered in time
+         * and also disables all buttons
+         * if timer reaches 0
+        */
+        if (count === 0) { 
             clearInterval(timer);
             addWrongAnswer();
             Array.from(answerButtons.children).forEach((button) => {
-                button.disabled = true; // Disable answer buttons when the time runs out
+                button.disabled = true; 
             });
 
-            // Show the next question button if 11 questions hasn´t been shown
+            
             if (11 > currentQuestionIndex + 1) {
                 nextQuestionButton.classList.remove("hide");
             }
         }
-    }, 1000); // timer set to count down by 1000milliseconds at the time (1sec)
+    }, 1000); 
 
-    // This will collect data from questions.js and will fill the question div with the question and create new buttons with the answers
-    questionContent.innerText = questions.question; // This puts in the question in the question div from the array in question.js
+    /** 
+     * This will collect data from 
+     * questions.js and will 
+     * fill the question div 
+     * with the question and 
+     * create new buttons 
+     * with the answers
+     * */ 
+    questionContent.innerText = questions.question; 
     questions.answers.forEach((answer) => {
-        const button = document.createElement("button"); // Will create new answerbuttons for each question.
+        const button = document.createElement("button"); 
         button.innerText = answer.text;
-        button.classList.add("answerbutton", "btn"); // Which classes to be set on the new buttons
+        button.classList.add("answerbutton", "btn"); 
         if (answer.correct) {
-            button.dataset.correct = answer.correct; // This will check what answer is correct and paste that fact to the button
+            button.dataset.correct = answer.correct; 
         }
-        button.addEventListener("click", selectAnswer); // Adds eventlisteners to the new buttons
+        button.addEventListener("click", selectAnswer); 
         answerButtons.appendChild(button);
     });
 }
 
 /**
- * This function will hide the button for the next question and remove the previous answering buttons because
- * they either have correct or wrong values. We want to start next question with new buttons 
+ * This function will hide 
+ * the button for the next question 
+ * and remove the previous answering 
+ * buttons because they either 
+ * have correct or wrong values. 
+ * We want to start 
+ * next question with 
+ * new and fresh buttons
  */
 function resetState() {
     nextQuestionButton.classList.add("hide");
@@ -241,45 +353,73 @@ function resetState() {
         answerButtons.removeChild(answerButtons.firstChild);
     }
 }
-// This function will take take the targeted answer and look if the answer are set with the value correct
+/** 
+ * This function will 
+ * take the targeted answer 
+ * and look if the answer 
+ * are set with the value correct
+ * or not and also
+ * disables all buttons when
+ * an answer has been selected
+ * */ 
 
 function selectAnswer(e) {
     clearInterval(timer);
     const selectedButton = e.target;
     const correct = selectedButton.dataset.correct;
 
-    Array.from(answerButtons.children).forEach((button) => { // disables all answer buttons when you´ve selected your answer
+    Array.from(answerButtons.children).forEach((button) => { 
         button.disabled = true;
     });
 
     Array.from(answerButtons.children).forEach((button) => {
-        // Clear all status classes from all buttons
-        clearStatusClass(button);
+     clearStatusClass(button);
     });
 
     if (correct === "true") {
-        // Add status class to the selected button if it´s correct or wrong answer
+        /** 
+         * Add status class to 
+         * the selected button 
+         * if it´s correct or 
+         * wrong answer
+         * */ 
         selectedButton.classList.add("correctanswer");
         addRightAnswer();
     } else {
         selectedButton.classList.add("wronganswer");
         addWrongAnswer();
     }
-
+        /** 
+         * Checks if 11 question 
+         * has been shown or not. 
+         * If 11 has been shown 
+         * the function finalScore() 
+         * will be called
+         * and the game ends
+         * */ 
     if (11 > currentQuestionIndex + 1) {
-        // Checks if 11 question has been shown or not. If 11 has been shown the function finalScore() will be called
         nextQuestionButton.classList.remove("hide");
     } else {
         finalScore();
     }
 
 }
-// This function will remove the class of right or wrong answer to the new answerbuttons
+/** 
+ * This function will remove 
+ * the class of right 
+ * or wrong answer 
+ * to the new answerbuttons
+ * */ 
 function clearStatusClass(button) {
     button.classList.remove("correctanswer");
     button.classList.remove("wronganswer");
 }
-// This function will increase the users score if answered correct
+/** 
+ * This function will increase 
+ * the users score if 
+ * correct answer has
+ * been selected
+ * */ 
 function addRightAnswer() {
     const correctScoreElement = document.querySelector("#scores .correct");
 
@@ -291,7 +431,12 @@ function addRightAnswer() {
     }
 }
 
-// This function will increase the quiz´s score if answered wrong
+/** 
+ * This function will increase 
+ * the quiz´s score if 
+ * selected answer
+ * are wrong
+ * */ 
 function addWrongAnswer() {
     const correctScoreElement = document.querySelector("#scores .wrong");
 
@@ -302,7 +447,11 @@ function addWrongAnswer() {
         correctScoreElement.innerText = newCorrectScore;
     }
 }
-// This function will reset the scoreboard if you restart the game
+/** 
+ * This function will reset 
+ * the scoreboard if 
+ * you restart the game
+ * */ 
 function resetScore() {
     const correctScoreElement = document.querySelector("#scores .correct");
     const wrongScoreElement = document.querySelector("#scores .wrong");
@@ -310,12 +459,20 @@ function resetScore() {
     correctScoreElement.innerText = "0";
     wrongScoreElement.innerText = "0";
 }
-// This function checks who has the highest score between user and quiz to send win or loss message at the end.
+/** 
+ * This function checks 
+ * who has the highest score 
+ * between user and quiz 
+ * to send win or 
+ * loss message at the end.
+ * */ 
 function finalScore() {
     resetButtons();
     endGameRestart();
-    const correctScore = parseInt(document.querySelector("#scores .correct").innerText);
-    const wrongScore = parseInt(document.querySelector("#scores .wrong").innerText);
+    const correctScore = 
+    parseInt(document.querySelector("#scores .correct").innerText);
+    const wrongScore = 
+    parseInt(document.querySelector("#scores .wrong").innerText);
 
     if (correctScore > wrongScore) {
         userWin.classList.remove("hide");
@@ -327,19 +484,27 @@ function finalScore() {
     yourRescentScore();
 }
 
-/** This will add the rescent score when the quiz ended
- * The rescent score will be shown where you put in the username before the quiz starts 
+/** This will add the rescent 
+ * score when the quiz ended
+ * to localstorage.
+ * The rescent score 
+ * will be shown where 
+ * you put in the username 
+ * before the quiz starts 
  * */
 function yourRescentScore() {
     const player = document.getElementById("username").textContent;
-    const playerScore = parseInt(document.querySelector("#scores .correct").innerText);
-    const quizScore = parseInt(document.querySelector("#scores .wrong").innerText);
+    const playerScore = 
+    parseInt(document.querySelector("#scores .correct").innerText);
+    const quizScore = 
+    parseInt(document.querySelector("#scores .wrong").innerText);
 
     const rescentPlayerElement = document.getElementById("rescent-player");
     const rescentScoreElement = document.getElementById("rescent-score");
 
     rescentPlayerElement.textContent = player;
-    rescentScoreElement.textContent = " scored " + playerScore + " - " + quizScore;
+    rescentScoreElement.textContent = 
+    " scored " + playerScore + " - " + quizScore;
 
     localStorage.setItem("latestUser", player);
     localStorage.setItem("latestScore", rescentScoreElement.textContent);
@@ -357,7 +522,13 @@ function yourRescentScore() {
 }
 
 
-// This function sends an email if all are fields are filled in and also sends a reply to the persons email via Emailjs
+/** 
+ * This function sends an email 
+ * if all are fields are filled 
+ * in and also sends 
+ * a reply to the persons 
+ * email via Emailjs
+ * */ 
 function sendEmail(event) {
     event.preventDefault();
     let params = {
@@ -385,13 +556,17 @@ function sendEmail(event) {
             .then(function() {
                 alert("Thank you for the feedback!");
 
-                // Reset the value of the forms input fields when feedback has been sent
+                /** 
+                 * Reset the value of 
+                 * the forms input fields 
+                 * when feedback has been sent
+                 * */ 
                 document.getElementById("name").value = "";
                 document.getElementById("email").value = "";
                 document.getElementById("message").value = "";
             })
             .catch(function(error) {
-                alert("Error sending email:", error); // Will send error message if feedback fails to be sent
+                alert("Error sending email:", error); 
 
             });
     }
